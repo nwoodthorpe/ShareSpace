a0477ea1334bd550c8bb8803254fdeca779d9d78
Nathaniel Woodthorpe <njwoodthorpe@gmail.com>
2017-01-22 13:48:30 -0800

    Do front page frontend stuff

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module ShareSpace
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths << Rails.root.join('lib')
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
